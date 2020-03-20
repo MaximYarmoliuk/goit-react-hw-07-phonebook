1a93ca32b15514fc11604eb03a8f6764c0961a34
Maxym <Jarmoluk97@gmail.com>
2020-03-20 15:46:56 +0200

    Updates

---
+++ precache-manifest.fa7dfc96e750756dda887eb65f8d3d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9805876255da018a6015d64497a16eb2",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "3178a4b7bb7873766e17",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.46e7156e.chunk.css"
+  },
+  {
+    "revision": "0a88b214cc2988d8d2f1",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.51a30b0a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.51a30b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3178a4b7bb7873766e17",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.f85cc167.chunk.js"
+  },
+  {
+    "revision": "4d1fc4d2a72bb2926934",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.a005afed.js"
+  }
+]);
